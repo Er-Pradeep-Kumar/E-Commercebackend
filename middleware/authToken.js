fe2f805ca80badc8330bc0977e473dfb0a29e31e
Pradeep Kumar <105978122+Er-Pradeep-Kumar@users.noreply.github.com>
2024-05-20 15:34:17 +0530

    token string given

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 async function authToken(req,res,next){
     try{
-        const token = req.cookies.token
+        const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjNiMDAwZmE1YTU4NTdlODFmZTQ4MGYiLCJlbWFpbCI6InByYWRlZXBrdW1hci5lci5vbmVAZ21haWwuY29tIiwiaWF0IjoxNzE2MTk5MjQ3LCJleHAiOjE3NDQ5OTkyNDd9.13tCY9T_EeZ8gDdjUIm1VQgpdPGibIfbnP3V1Y6k5eI"
 
         console.log("token",token)
         if(!token){

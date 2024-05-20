8f6533b8560de8ccbe1133c0296862bef446aba3
Pradeep Kumar <105978122+Er-Pradeep-Kumar@users.noreply.github.com>
2024-05-20 10:53:46 +0530

    3change

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/api/getkey",(req,res)=>
 res.status(200).json({key:process.env.RAZORPAY_API_KEY}))
 //payment
 
-const PORT = 8080 || process.env.PORT
+const PORT =process.env.PORT||8080 
 
 
 connectDB().then(()=>{
